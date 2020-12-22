295194b383e0ec14eb1d362d6084d58d29e55dc9
Barry Lapthorn <barry@lapthorn.net>
2020-12-22 18:00:24 +0000

    can't get jquery working

--- themes/r3-tailwind/assets/js/activate-tabs.js
+++ themes/r3-tailwind/assets/js/activate-tabs.js
@@ -1,7 +1,8 @@
 //  We want scripts to load last, including jquery.
 //  Once jquery is loaded we can then 'activate' all the tabs.
 
-import $ from "jquery";
+import $ from 'jquery';
+import jQuery from "jquery";
 import "jquery-ui";
 import "jquery-ui/ui/widgets/tabs";
 
@@ -12,30 +13,28 @@ import "jquery-ui/ui/widgets/tabs";
 
 const default_tab = "java";
 
-function activateTab(e) {
-
+export function activateTabs() {
+  var elements = document.getElementsByClassName("r3-o-tabs");
+  for (var i = 0; i < elements.length; i++) {
+    var e = elements[i];
     var s = "#" + e.id;
     var default_tab_name = s + "-" + default_tab;
 
     // https://getbootstrap.com/docs/4.4/components/navs/#tabshow
 
-    // Always make the first tab the visible one
-    $(s + " a")
-      .first()
-      .tab("show");
+    // // Always make the first tab the visible one
+    // $(s + " a")
+    //   .first()
+    //   .tab("show");
 
-    // Unless it's the default tab i.e. 'java', in which case, show that instead
-    var selector = s + ' a[href="' + default_tab_name + '"]';
-    $(selector).tab("show");
-}
-
-export function activateTabs() {
-  var elements = document.getElementsByClassName("r3-o-tabs");
-  for (var i = 0; i < elements.length; i++) {
-    var e = elements[i];
-    activateTab(e);
+    // // Unless it's the default tab i.e. 'java', in which case, show that instead
+    // var selector = s + ' a[href="' + default_tab_name + '"]';
+    // $(selector).tab("show");
   }
 }
 
 
-// https://stackoverflow.com/questions/31657981/how-to-create-a-simple-jquery-tabs-without-jquery-ui
+
+window.$ = $;
+window.jQuery = jQuery;
+
