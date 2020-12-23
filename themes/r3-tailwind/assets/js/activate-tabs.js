d17acc323dd2dd888ef930e3f9850246f23790e4
Barry Lapthorn <barry@lapthorn.net>
2020-12-23 13:55:51 +0000

    sort of working

--- themes/r3-tailwind/assets/js/activate-tabs.js
+++ themes/r3-tailwind/assets/js/activate-tabs.js
@@ -1,40 +1,51 @@
-//  We want scripts to load last, including jquery.
-//  Once jquery is loaded we can then 'activate' all the tabs.
+//  JQuery should load last so that the DOM is available etc.
 
-import $ from 'jquery';
-import jQuery from "jquery";
-import "jquery-ui";
-import "jquery-ui/ui/widgets/tabs";
+import $ from "jquery";
 
-// //  Importing bootstrap on its own adds an extra event handler to jquery
-// //  that breaks the dropdown toggle, i.e. the versions menu, so we have
-// //  to import only the component we want instead - the bootstrap-jquery-tab
-// import 'bootstrap/js/dist/tab';
+const DEFAULT_TAB_NAME = "java";
 
-const default_tab = "java";
+function activateTabSet(e) {
+  let id = "#" + e.id;
+  var defaultTabId = id + "-" + DEFAULT_TAB_NAME;
+  console.log("Default = " + defaultTabId);
+
+  var $tabSet = $(e.id);
+  var $tabs = $(id + " ul li");
+
+  // Hide all code panes - we unhide one of them later
+  $tabSet.find("tab-pane").each(function (index) {
+    $(this).addClass("hidden");
+  });
+
+  $tabs.each(function (index) {
+    var $tabLink = $(this).find("a");
+    var tabName = $tabLink.html();
+    var tabPaneId = id + "-" + tabName;
+
+    // add on click handler
+    $tabLink.on("click", function () {
+      // ensure all panes are hidden
+      $tabSet.find("tab-pane").each(function (index) {
+        console.log("Hiding ");
+        console.log(this);
+
+        $(this).addClass("hidden");
+      });
+      // except us when we're clicked.
+      $(tabPaneId).removeClass("hidden");
+    });
+
+    // unhide the tab pane.
+    if ($tabs.length === 1 || tabName === DEFAULT_TAB_NAME) {
+      $tabLink.addClass("text-bold underline");
+      $(tabPaneId).removeClass("hidden");
+    }
+  });
+}
 
 export function activateTabs() {
   var elements = document.getElementsByClassName("r3-o-tabs");
   for (var i = 0; i < elements.length; i++) {
-    var e = elements[i];
-    var s = "#" + e.id;
-    var default_tab_name = s + "-" + default_tab;
-
-    // https://getbootstrap.com/docs/4.4/components/navs/#tabshow
-
-    // // Always make the first tab the visible one
-    // $(s + " a")
-    //   .first()
-    //   .tab("show");
-
-    // // Unless it's the default tab i.e. 'java', in which case, show that instead
-    // var selector = s + ' a[href="' + default_tab_name + '"]';
-    // $(selector).tab("show");
+    activateTabSet(elements[i]);
   }
 }
-
-
-
-window.$ = $;
-window.jQuery = jQuery;
-
